95c833820441f5d3e6ffdd503511e8fd67115616
Cristian Ramirez <ccrhdbj@outlook.com>
2024-02-20 08:50:46 -0500

    Modifica los listeners para facilitar la lectura

--- src/utils/listeners.js
+++ src/utils/listeners.js
@@ -1,44 +1,71 @@
-import { AboutMe } from "../Pages/AboutMe"
-import { NewsPage } from "../Pages/News"
-import { NewsItem } from "../Pages/NewsItem"
-import { Contact } from "../templates/Contact/Contact"
-import { Footer } from "../templates/Footer/Footer"
-import Main from "../templates/Main/Main"
+import { AboutMe } from "../Pages/AboutMe";
+import { NewsPage } from "../Pages/News";
+import { NewsItem } from "../Pages/NewsItem";
+import { Contact } from "../templates/Contact/Contact";
+import { Footer } from "../templates/Footer/Footer";
+import Main from "../templates/Main/Main";
 
-
-
-const Listeners = (i) => {
-  document.body.addEventListener("click", (event)=>{
-    if(event.target.classList.contains("main--home-container--button")){
-      const toReplace = document.body.children[1]
-      
-      //convertir el return de newspage en un elemento nodo
-      document.body.classList.toggle("no-scroll")
-
-      document.body.replaceChild(NewsPage, toReplace)
+const Listeners = () => {
+  document.body.addEventListener("click", (event) => {
+    const e = event.target.classList;
+    if (e.contains("main--home-container--button")) {
+      onBodyScroll(true);
+      onBodyReplace("replace", NewsPage);
     }
-    if(event.target.classList.contains("blogs-button-container")){
-      const toReplace = document.body.children[1]
-      document.body.replaceChild(NewsItem, toReplace)
-      document.body.appendChild(Contact)
-      document.body.appendChild(Footer)
+    if (e.contains("blogs-button-container")) {
+      onBodyScroll(true);
+      onBodyReplace("replace", NewsItem);
     }
-    if(event.target.classList.contains("nav--container-profile-link--a")){
-      const toReplace = document.body.children[1]
-      document.body.replaceChild(AboutMe, toReplace)
-      document.body.appendChild(Contact)
-      document.body.appendChild(Footer)
+    if (e.contains("nav--container-profile-link--a")) {
+      onBodyScroll(true);
+      onBodyReplace("append", AboutMe);
     }
-    if(event.target.classList.contains("nav--container-logo--img")){
-      const toReplace = document.body.children[1]
-      document.body.replaceChild(Main, toReplace)
-      document.body.classList.toggle("no-scroll")
-      const toReplace3 = document.body.children[3]
-      const toReplace4 = document.body.children[4]
-      document.body.removeChild(toReplace4)
-      document.body.removeChild(toReplace3)
+    if (e.contains("nav--container-logo--img")) {
+      onBodyScroll(false);
+      onBodyReplace("remove", Main, [4,3]);
+    }
+  });
+  window.addEventListener('popstate', (event) => {
+    // Aquí puedes manejar el evento de retroceso
+    event.preventDefault();
+    alert('Se ha retrocedido en el historial del navegador');
+  });
+};
 
-    }
-  })
-}
-export default Listeners
+const onBodyScroll = (state) => {
+  const body = document.body;
+  if (state == true) {
+    body.classList.remove("no-scroll");
+  } else {
+    body.classList.add("no-scroll");
+  }
+};
+const onBodyReplace = (use, element, array=[]) => {
+  if (use == "replace") {
+    const toReplace = document.body.children[1];
+    document.body.replaceChild(element, toReplace);
+  } else if (use == "append") {
+    const toReplace = document.body.children[1];
+    document.body.replaceChild(element, toReplace);
+    document.body.appendChild(Contact);
+    document.body.appendChild(Footer);
+  }else if (use == "remove") {
+    const toReplace = document.body.children[1];
+    document.body.replaceChild(element, toReplace);
+    array.forEach((arg) => {
+      const toReplace = document.body.children[arg];
+      document.body.removeChild(toReplace)
+    });
+  }
+};
+export default Listeners;
+
+
+/* 
+
+Falta metodo para regresar a la pagina anterior
+
+
+
+
+*/
